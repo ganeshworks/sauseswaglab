388fa9b6268d8d2f20d34382f7725831a5349dbd
GK <ganeshvadla98@gmail.com>
2025-10-11 15:21:59 +0530

    added jekins file

--- src/test/java/testUtility/BaseClass.java
+++ src/test/java/testUtility/BaseClass.java
@@ -10,18 +10,37 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseClass {
 
 	public static WebDriver driver;
-	
+
+	/*
+	 * @BeforeSuite(alwaysRun = true) public void beforesuite() {
+	 * 
+	 * WebDriverManager.chromedriver().setup();
+	 * 
+	 * ChromeOptions options=new ChromeOptions();
+	 * 
+	 * options.addArguments("--incognito");
+	 * 
+	 * driver=new ChromeDriver(options);
+	 * 
+	 * driver.manage().window().maximize();
+	 */
 	@BeforeSuite(alwaysRun = true)
 	public void beforesuite() {
-		
 		WebDriverManager.chromedriver().setup();
-		
-		ChromeOptions options=new ChromeOptions();
-		
+
+		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--incognito");
-		
-		driver=new ChromeDriver(options);
-		
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--headless=new"); // run headless on Jenkins Linux
+		options.addArguments("--remote-allow-origins=*");
+
+		// 👇 unique directory for each run
+		options.addArguments("--user-data-dir=/tmp/chrome-user-data-" + System.currentTimeMillis());
+
+		driver = new ChromeDriver(options);
 		driver.manage().window().maximize();
 	}
+
 }
