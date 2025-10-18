ad8e9331c8fe3321e91cde50354e849d7fec9055
GK <ganeshvadla98@gmail.com>
2025-10-18 16:43:50 +0530

    added home page validations

--- src/test/java/testClasses/HomePageTestUI.java
+++ src/test/java/testClasses/HomePageTestUI.java
@@ -23,18 +23,78 @@ public class HomePageTestUI extends BaseClass {
 		test = extent.createTest("Home Page validations  ");
 
 		List<String> allitems = h.backpack();
-		// test.log(Status.INFO, "data is "+allitems.toString());
 		test.log(Status.INFO, "Data is: " + allitems.toString());
-
-		if (h.backpack().contains("Sauce Labs Backpack")) {
+		if (h.getdata()) {
 			test.log(Status.PASS, "Sause Backpack Was Available On Home Screen");
+		} else {
+			test.log(Status.FAIL, "Searched text Ws Not Available on Homepage");
 		}
+		// Click On Selected Linked Text
 
-		/*
-		 * Assert.assertTrue(h.backpack().contains("Sauce Labs Backpack"
-		 * ),"sause was not there"); test.log(Status.PASS,
-		 * "Sause Backpack Was Available On Home Screen");
-		 */
+		List<String> productNames = h.getProdNames();
+		// get all product names dynamically
+		for (String prod : productNames) {
+			h.clickProduct1(prod);
+			test.log(Status.PASS, "Clicked On : " + prod);
+			Thread.sleep(4000);
+			driver.navigate().back();
+			test.log(Status.PASS, "Navigated back To Screen");
+			Thread.sleep(2000);
+			
 
+		}
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//add small delay to ensure page reloads before nextiteration }
+
+			/*
+			 * h.clickProduct1("Sauce Labs Onesie"); test.log(Status.PASS,
+			 * "Clicked On : Sauce Labs Onesie"); driver.navigate().back();
+			 * test.log(Status.PASS, "Navigated back To Screen"); Thread.sleep(2000);
+			 * h.clickProduct1("Sauce Labs Bike Light"); test.log(Status.PASS,
+			 * "Clicked On : Sauce Labs Bike Light"); Thread.sleep(2000);
+			 * driver.navigate().back(); test.log(Status.PASS, "Navigated back To Screen");
+			 * h.clickProduct1("Sauce Labs Bolt T-Shirt"); test.log(Status.PASS,
+			 * "Clicked On : Sauce Labs Bolt T-Shirt"); Thread.sleep(2000);
+			 * driver.navigate().back(); test.log(Status.PASS, "Navigated back To Screen");
+			 * h.clickProduct1("Sauce Labs Fleece Jacket"); test.log(Status.PASS,
+			 * "Clicked On : Sauce Labs Fleece Jacket"); Thread.sleep(2000);
+			 * driver.navigate().back(); test.log(Status.PASS, "Navigated back To Screen");
+			 * h.clickProduct1("Sauce Labs Onesie"); test.log(Status.PASS,
+			 * "Clicked On : Sauce Labs Onesie"); Thread.sleep(2000);
+			 * driver.navigate().back(); test.log(Status.PASS, "Navigated back To Screen");
+			 * h.clickProduct1("Test.allTheThings() T-Shirt (Red)"); test.log(Status.PASS,
+			 * "Clicked On : Test.allTheThings() T-Shirt (Red)"); Thread.sleep(2000);
+			 * driver.navigate().back(); test.log(Status.PASS, "Navigated back To Screen");
+			 */
--- src/test/java/testobjects/HomeElementsUI.java
+++ src/test/java/testobjects/HomeElementsUI.java
@@ -1,12 +1,15 @@
 package testobjects;
 
 import java.lang.reflect.Array;
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomeElementsUI {
 
@@ -17,6 +20,9 @@ public class HomeElementsUI {
 	}
 
 	By backpack = By.id("inventory_container");
+	By items = By.xpath("//div[@class='inventory_list']");
+	By getelemet = By.className("inventory_item_name");
+	By listprodnames = By.xpath("//div[@class='inventory_item_name ']");
 
 	public List<String> backpack() throws Exception {
 
@@ -24,11 +30,13 @@ public class HomeElementsUI {
 		ArrayList<String> text = new ArrayList<String>();
 		for (WebElement items : all) {
 			text.add(items.getText());
-
 		}
-		System.out.println(text);
-		Thread.sleep(4000);
 		return text;
+	}
+
+	public boolean getdata() throws Exception {
+		boolean test = backpack().stream().anyMatch(item -> item.toLowerCase().contains("sauce labs backpack"));
+		return test;
 	}
 
 	public String getbackpack() {
@@ -36,4 +44,36 @@ public class HomeElementsUI {
 		System.out.println();
 		return back;
 	}
+
+	public String getlistofitems() {
+		return driver.findElement(items).getText();
+	}
+
+	public void clickProduct1(String names) {
+		List<WebElement> productNames = driver.findElements(listprodnames);
+
+		// Step 2: Loop through and find "Sauce Labs Backpack"
+		for (WebElement product : productNames) {
+			String name = product.getText().trim();
+			
+			if (name.contains(names)) {
+				// Step 3: Click the product using its WebElement
+				product.click(); // Direct click on the link text WebElement
+				break; // stop after clicking
+				
+			}
+		}
+
+	}
+
+	public List<String> getProdNames() {
+		List<WebElement> prodElements = driver.findElements(listprodnames);
+		List<String> names = new ArrayList<>();
+
+		for (WebElement element : prodElements) {
+			names.add(element.getText().trim());
+		}
+		return names;
+	}
+
 }
